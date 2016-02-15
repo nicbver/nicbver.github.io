74aabbfd34f27a8a0d2a6b73951d68fbe6879df4
Your Name <you@example.com>
2016-02-14 21:34:40 -0500

    Inital Commit

---
+++ Documents/insta_sign_in.rb
@@ -0,0 +1,15 @@
+#mock instagram sign up
+
+puts "enter email to create account:"
+
+email = gets
+
+puts "now create a password for #{email}:"
+
+password = gets
+
+
+puts "your login in is #{email}"
+puts "and your password is #{password}"
+
+puts "Welcome to Instagram!"
